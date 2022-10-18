cb08443aaa1dcae55c1e44e58c897f20f328a6a7
y3n <y3ndev@gmail.com>
2022-10-18 19:17:34 +0200

    correction bug credit

--- src/server.c
+++ src/server.c
@@ -41,6 +41,8 @@ void handle_client(int c_sock, struct sockaddr_in *c_addr_ptr) {
 
   size_t bytes_read;
   while (1) {
+    unsigned int credit = last_sent_seg_no - last_received_ack_no;  // nb of unacknowledged segments
+
     char *seg = window[last_sent_seg_no % window_size];
 
     snprintf(seg, ACK_NO_LENGTH + 1, "%06d", last_sent_seg_no);  // ACK_NO_LENGTH + 1 to include null terminator
@@ -52,24 +54,28 @@ void handle_client(int c_sock, struct sockaddr_in *c_addr_ptr) {
 
     // send data and save in seg_buffer
     if (bytes_read > 0) {
+      printf("sending segment %d (%ld bytes)\n", last_sent_seg_no, ACK_NO_LENGTH + bytes_read);
       send_bytes(c_sock, seg, ACK_NO_LENGTH + bytes_read, c_addr_ptr);
     }
 
     // poll for ACK, or if window is full, wait for ACK until timeout
-    unsigned int credit = last_sent_seg_no - last_received_ack_no;  // nb of unacknowledged segments
     if (credit > window_size) {
-      printf("Error: credit = %d = %d - %d > window_size\n", credit, last_sent_seg_no, last_received_ack_no);
+      printf("Error: credit = %d = %d - %ld > window_size\n", credit, last_sent_seg_no, last_received_ack_no);
       exit(1);
     }
     printf("credit = %d\n", credit);
     struct timeval *time_ptr = credit == window_size ? &timeout : &polling;
-    printf("select using timeout ? %d\n", time_ptr == &timeout);
+    //printf("select using timeout ? %d\n", time_ptr == &timeout);
+    if(time_ptr == &timeout) printf("waiting for ack because window is full..\n");
     FD_SET(c_sock, &read_set);
     select(c_sock + 1, &read_set, NULL, NULL, time_ptr);
+    int received_ack = 0;
     if (FD_ISSET(c_sock, &read_set)) {
+      //printf("expect ACK\n");
       // expect ACK
       char msg[3 + ACK_NO_LENGTH];
       size_t n = recv_str(c_sock, msg, c_addr_ptr);
+      //printf("recv: %s\n", msg);
 
       if (strncmp(msg, ACK, 3) == 0) {
         unsigned int ack = atoi(&msg[3]);
@@ -78,24 +84,31 @@ void handle_client(int c_sock, struct sockaddr_in *c_addr_ptr) {
           break;
         }
 
+        //printf("ack > last_received_ack_no => %d > %ld\n", ack, last_received_ack_no);
         if (ack > last_received_ack_no) {
           print_ts();
           printf("RECEIVED ACK %d\n", ack);
           last_received_ack_no = ack;
+          received_ack = 1; // cancel timeout trigger
         }
       } else {
         print_ts();
         printf("Expected ACK, got %s\n", msg);
       }
-    } else if (time_ptr == &timeout) {
+    }
+    
+    if (time_ptr == &timeout && !received_ack) {
       // timeout : no ACK received for window, resend since last_ack_received
       printf("Timeout\n");
+      // edit credit
       last_sent_seg_no = last_received_ack_no;
+      printf("resending from seg %d new credit = %ld\n", last_sent_seg_no+1, last_sent_seg_no - last_received_ack_no);
     } /* else {
        // no ACK received, but window is not full
      }*/
 
-    if (bytes_read > 0) {
+    // it should not be incremented if it's the last segment
+    if (bytes_read == FILE_CHUNK_SIZE) {
       last_sent_seg_no++;
     }
   }
